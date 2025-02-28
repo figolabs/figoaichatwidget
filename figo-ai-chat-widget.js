76a3a912e953fd5531dc2ac3476761da44209aa0
Ugochukwu Okoli <51544977+Okoli-Ryan@users.noreply.github.com>
2025-02-28 02:56:04 +0100

    Update figo-ai-chat-widget.js: add allow forms permission

--- figo-ai-chat-widget.js
+++ figo-ai-chat-widget.js
@@ -52,7 +52,7 @@
 		iframe.className = "figo-chat-iframe";
 		iframe.style = "width: 100%; height: 100%; border: none;";
 		iframe.allow = "microphone; clipboard-write; clipboard-read";
-		iframe.sandbox = "allow-scripts  allow-same-origin";
+		iframe.sandbox = "allow-scripts allow-forms allow-same-origin";
 
 		setTimeout(() => {
 			injectIframeStyles();
